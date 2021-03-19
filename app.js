cee5cad9e213d526643d44c967aad4408112ea10
oscarmora26 <oscar.rander@outlook.com>
2021-03-19 10:19:54 -0400

    se agrego jorge

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const colaboradores = [
     {nombre: 'Rander', correo: 'rander@gmail.com'},
-    {nombre: 'Oscar', correo: 'oscar@gmail.com'},
+    {nombre: 'Jorge', correo: 'jorge@gmail.com'},
     {nombre: 'Oscar', correo: 'oscar@gmail.com'},
     {nombre: 'Oscar', correo: 'oscar@gmail.com'},
     {nombre: 'Oscar', correo: 'oscar@gmail.com'},
